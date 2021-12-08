087effef09f3f4bf3c84f850ac0cc6c07f5fd636
Amin Sehati <Aminsehati70@gmail.com>
2021-12-08 19:27:53 +0330

    dix bug listen app

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ app.get("/",(req,res)=>{
 
 const port = process.env.PORT || 5000 
 const server = http.createServer(app);
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
 
 module.exports = app;
